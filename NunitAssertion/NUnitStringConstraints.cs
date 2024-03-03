9b73f0cff885d16f6a70f39b1ed328d218cf485c
Deniz Uzundağ <uzundagdeniz@gmail.com>
2024-03-03 12:07:59 +0300

    Proje dosyası ekle.

---
+++ NunitAssertion/NUnitStringConstraints.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitAssertion
+{
+    public class NUnitStringConstraints
+    {
+        string message1 = "Hello, friends! Welcome to my world.";
+        string message2 = "HELLO, FRIENDS! WELCOME TO MY WORLD.";
+        string message3 = string.Empty;
+
+        [Test]
+        public void StringEmpty()
+        {
+            //String.Empty
+            Assert.That(message1, Is.Not.Empty);
+            Assert.That(message3, Is.Empty);
+
+        }
+
+        [Test]
+        public void NullOrEmpty()
+        {
+            //String.NullOrEmpty:
+            Assert.That(message1, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void IsEqualTo()
+        {
+            Assert.That(message1, Is.EqualTo("Hello, friends! Welcome to my world."));
+            Assert.That(message2, Is.Not.EqualTo("Hello, friends! Welcome to my world."));
+            Assert.That(message2, Is.Not.EqualTo(message1));
+            Assert.That(message2[0], Is.EqualTo('H'));
+        }
+
+        [Test]
+        public void StartWith()
+        {
+            Assert.That(message1, Does.StartWith("Hello, friends!"), "Does not StartWith ");
+        }
+
+        [Test]
+        public void EndWith()
+        {
+            Assert.That(message2, Does.EndWith("WORLD."));
+            Assert.That(message2, Does.Not.EndWith("my world."));
+        }
+
+        [Test]
+        public void Contains()
+        {
+            //String.Contain
+            Assert.That(message2, Does.Contain("TO MY"));
+            Assert.That(message1, Does.Not.Contain("welcome"));
+            Assert.That(message1, Does.Contain("friends!").And.Contain("my"));
+        }
+
+        [Test]
+        public void DoesMatch()
+        {
+            Assert.That(message1, Does.Match("l*l"));
+        }
+
+        [Test]
+        public void LenghtControl()
+        {
+            //String.Length
+            Assert.That(message1.Length, Is.GreaterThan(10));
+        }
+
+    }
+}
---
+++ NunitAssertion/NunitCollectionConstraints.cs
@@ -0,0 +1,82 @@
+﻿
+namespace NunitAssertion
+{
+    public class NunitCollectionConstraints
+    {
+        int[] collectionX = { 1, 3, 5, 9, 12 };
+        int[] collectionY = { 1, 3, 5, 9, 12 };
+        int[] collectionZ = { 4, 8, 11, 2, 6, 2 };
+        int[] collectionW = null;
+
+        [Test]
+        public void HasMember()
+        {
+            Assert.That(collectionX, Has.Member(3));
+        }
+
+        [Test]
+        public void IsEqualTo()
+        {
+            Assert.That(collectionX, Is.EqualTo(collectionY), "Collections are not equal");
+        }
+
+        [Test]
+        public void IsNotEqualTo()
+        {
+            Assert.That(collectionX, Is.Not.EqualTo(collectionZ), "Collections are not equal");
+        }
+
+        [Test]
+        public void CountAssertion()
+        {
+            Assert.That(collectionX.Count, Is.EqualTo(5));
+            //Assert.That(collectionY,Has.Count(5));
+        }
+
+        [Test]
+        public void ContainsAssertion()
+        {
+            Assert.That(collectionX, Contains.Item(3));
+            Assert.That(collectionX, Does.Not.Contain(2));
+        }
+
+        [Test]
+        public void GreaterThanAssertion()
+        {
+            // Check if all elements are greater than 0
+            Assert.That(collectionZ, Is.All.GreaterThan(0));
+        }
+
+        [Test]
+        public void LessThanAssertion()
+        {
+            // Check if all elements are less than 10
+            Assert.That(collectionY, Is.All.LessThan(15));
+        }
+
+        [Test]
+        public void IsNotEmpty()
+        {
+            Assert.That(collectionZ, Is.Not.Empty);
+        }
+
+        [Test]
+        public void IsAllNotNull()
+        {
+            Assert.That(collectionY, Is.All.Not.Null);
+        }
+
+        [Test]
+        public void IsNull()
+        {
+            Assert.That(collectionW, Is.Null);
+        }
+
+        [Test]
+        public void UniqueItems()
+        {
+            Assert.That(collectionX, Is.Unique);
+            Assert.That(collectionZ, Is.Not.Unique);
+        }
+    }
+}
